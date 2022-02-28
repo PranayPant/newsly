7d64fa33be15c532a9ad42ad79cc098fcc7e3ade
PranayPant <ppantg1@gmail.com>
2022-02-27 20:40:25 -0600

    refactored articles

---
+++ components/Card/Card.js
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types'
+
+const Card = ({
+    article: { author, title, description, url, urlToImage, publishedAt },
+}) => {
+    if (!urlToImage || !title || !description || !publishedAt) {
+        return <></>
+    }
+    const modPublishedAt = new Date(Date.parse(publishedAt)).toLocaleTimeString(
+        'en-US',
+        {
+            hour12: true,
+            hour: '2-digit',
+            minute: '2-digit',
+        }
+    )
+    return (
+        <div className="sm:w-96 w-72 m-2 p-3 bg-gray-200 rounded-lg flex-auto flex flex-col">
+            <img className="rounded-lg" src={urlToImage} alt={title}></img>
+            <div className=" flex flex-col flex-auto pt-3">
+                <a
+                    className="text-2xl hover:text-blue-700 hover:underline"
+                    href={url}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    {title}
+                </a>
+
+                <span className="flex-auto pt-5 text-lg">{description}</span>
+                <span className="pt-3 inline-flex self-end text-xs">
+                    Updated at {modPublishedAt}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    article: PropTypes.shape({
+        source: PropTypes.shape({
+            id: PropTypes.string,
+            name: PropTypes.string,
+        }),
+        author: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+        urlToImage: PropTypes.string,
+        publishedAt: PropTypes.string,
+        content: PropTypes.string,
+    }).isRequired,
+}
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import Head from 'next/head'
+
 import headlinesApi from '@queries/headlines'
+import Card from '@components/Card'
 
 export default function Home({ data: { articles } }) {
     return (
@@ -16,61 +18,12 @@ export default function Home({ data: { articles } }) {
                 <h1>Newsly: The News Hub</h1>
             </header>
             <main className="flex flex-wrap justify-evenly">
-                {articles.map(
-                    ({
-                        author,
-                        title,
-                        description,
-                        url,
-                        urlToImage,
-                        publishedAt,
-                    }) => {
-                        if (
-                            !urlToImage ||
-                            !title ||
-                            !description ||
-                            !publishedAt
-                        ) {
-                            return <></>
-                        }
-                        const modPublishedAt = new Date(
-                            Date.parse(publishedAt)
-                        ).toLocaleTimeString('en-US', {
-                            hour12: true,
-                            hour: '2-digit',
-                            minute: '2-digit',
-                        })
-                        return (
-                            <div
-                                className="sm:w-96 w-72 m-2 p-3 bg-gray-200 rounded-lg flex-auto flex flex-col"
-                                key={`${author}${title}`}
-                            >
-                                <img
-                                    className="rounded-lg"
-                                    src={urlToImage}
-                                    alt={title}
-                                ></img>
-                                <div className=" flex flex-col flex-auto pt-3">
-                                    <a
-                                        className="text-2xl hover:text-blue-700 hover:underline"
-                                        href={url}
-                                        target="_blank"
-                                        rel="noreferrer noopener"
-                                    >
-                                        {title}
-                                    </a>
-
-                                    <span className="flex-auto pt-5 text-lg">
-                                        {description}
-                                    </span>
-                                    <span className="pt-3 inline-flex self-end text-xs">
-                                        Updated at {modPublishedAt}
-                                    </span>
-                                </div>
-                            </div>
-                        )
-                    }
-                )}
+                {articles.map((article) => (
+                    <Card
+                        key={`${article.author}${article.title}`}
+                        article={article}
+                    />
+                ))}
             </main>
         </div>
     )
@@ -80,21 +33,7 @@ Home.propTypes = {
     data: PropTypes.shape({
         status: PropTypes.string,
         totalResults: PropTypes.number,
-        articles: PropTypes.arrayOf(
-            PropTypes.shape({
-                source: PropTypes.shape({
-                    id: PropTypes.string,
-                    name: PropTypes.string,
-                }),
-                author: PropTypes.string,
-                title: PropTypes.string,
-                description: PropTypes.string,
-                url: PropTypes.string,
-                urlToImage: PropTypes.string,
-                publishedAt: PropTypes.string,
-                content: PropTypes.string,
-            })
-        ),
+        articles: PropTypes.arrayOf(Card.propTypes.article),
     }).isRequired,
 }
 
