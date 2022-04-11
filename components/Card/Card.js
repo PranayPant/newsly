d2c999d8805311aab8828de0e3eb5166fe57c2bb
PranayPant <ppantg1@gmail.com>
2022-04-11 15:26:08 -0500

    wip

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -57,7 +57,9 @@ const Card = ({
                 <div className="flex pt-3">
                     <div className="flex-auto">
                         <Socials
-                            url={`https://newsapp.cf/articles/${title}?content=${content}&description=${description}&url=${url}&urlToImage=${urlToImage}`}
+                            url={encodeURIComponent(
+                                `https://newsapp.cf/articles/${title}?content=${content}&description=${description}&url=${url}&urlToImage=${urlToImage}`
+                            )}
                             title={title}
                         />
                     </div>
